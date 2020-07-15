a06b2fbff57b8b13f34132bcc2d940921b1a534a
mhero007 <momohhero2@gmail.com>
2020-07-15 20:35:36 +0100

    app template and styling, sign in functionality implemented

--- src/router/index.js
+++ src/router/index.js
@@ -7,17 +7,43 @@ Vue.use(VueRouter);
 const routes = [
   {
     path: "/",
-    name: "Home",
+    name: "home",
     component: Home
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: "/login",
+    name: "login",
+    component: () => import("@/views/Login.vue")
+  },
+  {
+    path: "/register",
+    name: "register",
+    component: () => import("@/views/Register.vue")
+  },
+  {
+    path: "/settings",
+    name: "settings",
+    component: () => import("@/views/Settings.vue")
+  },
+  {
+    path: "/editor",
+    name: "editor_new",
+    component: () => import("@/views/ArticleCreate.vue")
+  },
+  {
+    path: "/editor/:article-slug",
+    name: "editor_edit",
+    component: () => import("@/views/ArticleEdit.vue")
+  },
+  {
+    path: "/editor/article/:article-slug",
+    name: "article",
+    component: () => import("@/views/Article.vue")
+  },
+  {
+    path: "/:username",
+    name: "profile",
+    component: () => import("@/views/Profile.vue")
   }
 ];
 
---
+++ src/views/Login.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="auth-page">
+    <div class="container page">
+      <div class="row">
+        <div class="col-md-6 offset-md-3 col-xs-12">
+          <h1 class="text-xs-center">Sign in</h1>
+          <p class="text-xs-center">
+            <a href="">Need an account?</a>
+          </p>
+
+          <ul class="error-messages">
+            <li v-for="error in this.errors" v-bind:key="error">
+              {{ error.message }}
+            </li>
+          </ul>
+
+          <form>
+            <fieldset class="form-group">
+              <input
+                v-model="email"
+                class="form-control form-control-lg"
+                type="text"
+                placeholder="Email"
+              />
+            </fieldset>
+            <fieldset class="form-group">
+              <input
+                v-model="password"
+                class="form-control form-control-lg"
+                type="password"
+                placeholder="Password"
+              />
+            </fieldset>
+            <button @click="login" class="btn btn-lg btn-primary pull-xs-right">
+              Sign in
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      password: "",
+      email: "",
+      errors: []
+    };
+  },
+  methods: {
+    login() {
+      this.$store
+        .dispatch("users/loginUser", {
+          email: this.email,
+          password: this.password
+        })
+        .then(() => {
+          this.errors = [];
+        })
+        .catch(err => {
+          this.errors.push(err);
+          console.log(err);
+        });
+    }
+  }
+};
+</script>
+>
